e9c1196a8336a0da35fcd6a0b2e4443e4c16f561
shivanshu1512 <shivanshu1512@gmail.com>
2025-11-15 23:51:38 +0530

    Add solution2.cpp for Codeforces problem CP-31

---
+++ CP-31/800/solution3.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        string s;
+        cin>>s;
+        bool threedot=false;
+        int total=0;
+        
+        for(int i=0;i<n;i++){
+            if(s[i]=='.' && i+1<n && s[i+1]=='.'&& i+2<n && s[i+2]=='.'){
+                threedot=true;
+                break;
+            }
+            if(s[i]=='.')
+                total++;
+        }
+        if(threedot)
+            cout<<2<<endl;
+        else
+            cout<<total<<endl;
+    }
+    return 0;
+}
+ 
