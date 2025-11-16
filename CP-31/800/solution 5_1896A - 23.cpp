41a9fdbe81c2c7dd4ae90349ca5f6dacb8d7f626
shivanshu1512 <shivanshu1512@gmail.com>
2025-11-16 17:33:21 +0530

    Add solution3.cpp for Codeforces problem CP-31

---
+++ CP-31/800/solution 5_1896A - 23.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        vector<long long> arr(n);
+        vector<long long> shiv(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+            if(arr[0]==1)
+                cout<<"YES"<<endl;
+            else
+                cout<<"NO"<<endl;
+        
+        
+        // shiv=sort(arr.begin(),arr.end());
+        // for(int i=1;i<n;i++){
+        //     if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+        //         int temp=arr[i];
+        //         arr[i]=arr[i+1];
+        //         arr[i+1]=temp;
+        //     }
+        // }
+    }
+    return 0;
+}
